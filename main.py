874cc05e5dcd165e039e55506a707e699c64d659
ljdyer <74581995+ljdyer@users.noreply.github.com>
2021-12-20 18:17:27 +0000

    updated

--- main.py
+++ main.py
@@ -1,93 +1,91 @@
 
-# Adapated from https://stackoverflow.com/a/43347984/17568469
+# Adapted from the following sources:
+# https://medium.com/analytics-vidhya/understanding-the-mel-spectrogram-fca2afa2ce53
+# https://stackoverflow.com/a/43347984/17568469
 
-import numpy
+import math
+import numpy as np
+import librosa
+import librosa.display
 import matplotlib.pyplot as plt
-import pylab
-from scipy.io import wavfile
-from scipy.fftpack import fft
 
-myAudio = "LRMonoPhase4.wav"
+MY_FILE = "comp_ling_fun.wav"
 
-# Read file to get sampling frequency and array of sound data sound_data is a
-# 'numpy.ndarray' object that looks like:
-# [[ 1  1] [ 0 -1] [ 1  0] ... [ 0  0] [ 9 -8] [ 6 -3]]
-sampling_frequency, sound_data = wavfile.read(myAudio)
+# ==============================================================================
 
-# TODO Delete this Check if wave file is 16bit or 32 bit. 24bit is not
-# supported sound_data_type = sound_data.dtype print(sound_data_type)
+# Get the audio time series and sample rate from the .wav file
+sound_data, sample_rate = librosa.load(MY_FILE)
 
-# Convert array of sound data to floating point values between -1 and 1.
-# It now looks like:
-# [[ 3.05175781e-05  3.05175781e-05] [ 0.00000000e+00 -3.05175781e-05] ...
-sound_data = sound_data / (2.**15)
+# Display sampling rate
+print(sample_rate)
 
-# Get the number of data points in the sample
-sample_points = float(sound_data.shape[0])
-# Get the duration of the sound file in seconds
-signal_duration = sample_points / sampling_frequency
+# Quantize to 8-bit audio (convert amplitude measures to integers between -128
+# and 127)
+sound_data_quantized = librosa.mu_compress(sound_data, quantize=True)
 
-# If there are two channels, select only one
-sound_data_one_channel = sound_data[:,0]
+sample_points = len(sound_data)                         # Number of sample points
+signal_length = sample_points / sample_rate             # Length of sound file in seconds
+point_duration = signal_length * 1000 / sample_points   # Duration of an sample point
+                                                        # in ms
 
+# Generate list of time points (in ms) to display on x axis
+time_points = [ x / sample_rate * 1000 for x in range(sample_points)]
 
-# === Plot raw waveform ===
+quantized_title = f"Your {round(signal_length, 3)}s audio recording quantized " + \
+                  "as 8-bit integer values."
 
-# Plot pressure values against time axis
-
-# Create an array of sample point in one dimension
-time_array = numpy.arange(0, sample_points, 1)
-time_array = time_array / sampling_frequency
-# Scale to milliSeconds
-time_array = time_array * 1000
-print(time_array)
-
-# Plot the tone
-plt.plot(time_array, sound_data_one_channel, color='green')
+plt.plot(time_points, sound_data)
+plt.title(quantized_title)
 plt.xlabel('Time (ms)')
 plt.ylabel('Amplitude')
 plt.show()
 
+# ==============================================================================
 
-# === Plot frequency content ===
+"""Get spectrum by converting the signal from the time domain into the
+frequency domain by Fourier transform
 
-# Get frequency from amplitude and time using Fast Fourier Transform (FFT)
-# algorithm to compute the discrete Fourier transform (DFT).
-# https://en.wikipedia.org/wiki/Fast_Fourier_transform
+From http://librosa.org/doc/main/generated/librosa.stft.html: Short-time
+Fourier transform (STFT). The STFT represents a signal in the time-frequency
+domain by computing discrete Fourier transforms (DFT) over short overlapping
+windows."""
 
-# Get the length of the sound_data object array
-sound_data_length = len(sound_data)
-# Get the Fourier transformation of each sample point
-fft_array = fft(sound_data)
-fft_array = fft(sound_data_one_channel)
-# Get the number of unique points
-num_unique_points = int(numpy.ceil((sound_data_length + 1) / 2.0))
-print(num_unique_points)
-fft_array = fft_array[0:num_unique_points]
+# n_fft is length of windowed signal after padding with zeroes. Librosa
+# recommends n_fft=512 for speech processing, which corresponds to 23
+# miliseconds at a sample rate of 22050 Hz (the default target sample rate for
+# librosa.load).
 
-# FFT contains both magnitude and phase, given in complex numbers in real +
-# imaginary parts (a + ib).
-# Take the absolute value to get only the real part.
-fft_array = abs(fft_array)
+n_fft = 512
+fft_start = math.floor(sample_points / 2)   # Start in the middle to maximise
+                                            # chance of catching some speech
+fft_end = fft_start + n_fft
 
-# Scale the fft array by number of sample points so that the magnitude does
-# not depend on the length of the signal or on its sampling frequency
-fft_array = fft_array / float(sound_data_length)
-# FFT has both positive and negative information. Square to get positive only
-fft_array = fft_array **2
+# Get duration and position to display to user
 
-if sound_data_length % 2 > 0:
-    # Odd number of points in FFT
-    fft_array[1 : len(fft_array)] = fft_array[1 : len(fft_array)] * 2
-else:
-    # Even number of points in FFT
-    fft_array[1 : len(fft_array) - 1] = fft_array[1 : len(fft_array) - 1] * 2
+# ====================
+def ms_to_sec_display(ms: float) -> str:
+    """Convert time in miliseconds to string in format mm:ss.sss"""
 
-freq_array = numpy.arange(0, num_unique_points, 1.0) * (sampling_frequency / sound_data_length)
+    s = round(ms/1000, 3)
+    m, s = divmod(s, 60)
+    before_dp, after_dp = divmod(s, 1)
+    mins = str(int(m)).zfill(2)
+    secs_before_dp = str(int(before_dp)).zfill(2)
+    secs_after_dp = str(after_dp)[2:5]
+    return f'{mins}:{secs_before_dp}.{secs_after_dp}'
 
-# Plot the frequency
-plt.plot(freq_array / 1000, 10 * numpy.log10(fft_array), color='blue')
-plt.xlabel('Frequency (Khz)')
-plt.ylabel('Power (dB)')
+time_start = time_points[fft_start]
+time_end = time_points[fft_end]
+duration = round(time_end - time_start)
+spectrum_title = f"Spectrum of the {duration}ms window of audio at " + \
+                 f"{ms_to_sec_display(time_start)}-{ms_to_sec_display(time_end)}"
+
+# Get spectrum
+spectrum = np.abs(librosa.stft(sound_data[fft_start:fft_end],
+                               n_fft=n_fft, hop_length = n_fft+1))
+
+plt.plot(spectrum)
+plt.title(spectrum_title)
+plt.xlabel('Frequency bin')
+plt.ylabel('Amplitude')
 plt.show()
-
